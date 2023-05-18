3a00bf88ad44c773be8d9cdda65c8d8931d1e349
Darren Harris <theharriscode401@gmail.com>
2023-05-18 09:52:16 -0700

    C file that puts code in standard output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - putting a line to S/O
+ * Return: Always 0 (Succss)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
