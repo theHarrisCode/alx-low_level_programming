dbaa70c608d9e36e3db97b69da57747aa8e93e88
Darren Harris <theharriscode401@gmail.com>
2023-05-18 09:59:37 -0700

    C file that prints out some text w/ output file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints out some text
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("With proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
