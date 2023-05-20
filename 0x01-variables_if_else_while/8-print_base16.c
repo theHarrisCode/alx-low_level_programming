48a0fc19972a592ca6a541d95bbbcd0218d39f27
Darren Harris <theharriscode401@gmail.com>
2023-05-20 13:31:15 -0700

    c file that prints all lowercase using hexidecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program that prints all the numbers of of base 16 in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 0x61;
+	while (c <= 0x7A)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
