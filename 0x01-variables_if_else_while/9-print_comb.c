343dcc67cc76657dfb7578244dc26321e18bd081
Darren Harris <theharriscode401@gmail.com>
2023-05-20 14:03:42 -0700

    single digit combos

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 57; i++)
+	{
+		if (i == '9')
+		{
+			putchar(i);
+		}
+		else
+		{
+			putchar(i);
+			putchar(',');
+		}
+	}
+	putchar('\n');
+}
