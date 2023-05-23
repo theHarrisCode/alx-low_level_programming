b2533c6b89f8b1915cf13800586f1af4e3e1f583
Darren Harris <theharriscode401@gmail.com>
2023-05-23 09:08:35 -0700

    checks if alphabet is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * islower - checks if character is a lowercase letter
+ * _islower - checks if character is a lowercase letter
  * @c: character to be checked
  * Return: 1 (Success) 0 (None Found)
  */
