d6c37df1d245e9838d610e2d334785a9024bb6d3
Darren Harris <theharriscode401@gmail.com>
2023-05-23 08:45:51 -0700

    prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints lowercase alphabet w/newline 10 times
+ * Return: None (Void)
+ * Parameters: None
+ */
+void print_alphabet_x10(void)
+{
+	/**
+	 * This program will act the same as printing alphabet but now enclosed
+	 * in a for loop.
+	 */
+	int i, j;
+
+	j = 'a';
+	for (i = 0; i < 9; i++)
+	{
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
