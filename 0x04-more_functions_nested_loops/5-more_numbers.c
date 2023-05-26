74a90f6cda707521206f7d8dd745fc40a1c3c8b5
Darren Harris <theharriscode401@gmail.com>
2023-05-25 18:38:42 -0700

    prints 0-14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0 to 14 ten times
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			if (i < 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			j++;
+		}
+		_putchar('\n');
+	}
+}
