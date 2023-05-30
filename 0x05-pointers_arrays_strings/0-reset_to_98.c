0f4496153eddce0837c70d293e04740a650999c5
Darren Harris <theharriscode401@gmail.com>
2023-05-30 11:38:15 -0700

    pointers!!!

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 =  resets variable to 98 using pointer
+ * reset_to_98 -  resets variable to 98 using pointer
  * @n: pointer to be manipulated
  */
 void reset_to_98(int *n)
