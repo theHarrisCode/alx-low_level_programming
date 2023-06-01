f1a8c35ed5c1e6ed6e29f1272ea13364fa8eb1e6
Darren Harris <theharriscode401@gmail.com>
2023-05-31 18:00:22 -0700

    copies to new dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,13 +7,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i, j;
 
 	i = 0;
 	while (src[i] != '\0')
 	{
-		*(dest + i) = src[i];
 		i++;
 	}
+	for (j = 0; j <= n; j++)
+	{
+		*(dest + j) = src[j];
 	return (dest);
 }
