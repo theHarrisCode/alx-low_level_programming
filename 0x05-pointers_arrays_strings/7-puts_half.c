121055556e1d0b6e49ba138307a9050ecccd0dba
Darren Harris <theharriscode401@gmail.com>
2023-05-31 17:07:55 -0700

    prints half of strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - prints half of the string
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i/2; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
