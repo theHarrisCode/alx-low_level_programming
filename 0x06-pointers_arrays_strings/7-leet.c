d5298f778be60be564fc9be43863f97594decdb5
Darren Harris <theharriscode401@gmail.com>
2023-06-02 16:08:13 -0700

    1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * leet - function that encode a string into 1337
+ * str - string to be manipulated
+ * Return: @str post-manipulation
+ */
+char *leet(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) == 'a' || *(str + i) == 'A')
+		{
+			*(str + i) = '4';
+		}
+		else if (*(str + i) == 'e' || *(str + i) == 'E')
+		{
+			*(str + i) = '3';
+		}
+		else if (*(str + i) == 'o' || *(str + i) == 'O')
+		{
+			*(str + i) = '0';
+		}
+		else if (*(str + i) == 't' || *(str + i) == 'T')
+		{
+			*(str + i) = '7';
+		}
+		else if (*(str + i) == 'l' || *(str + i) == 'L')
+		{
+			*(str + i) = '1';
+		}
+		i++;
+	}
+	return (str);
+}
