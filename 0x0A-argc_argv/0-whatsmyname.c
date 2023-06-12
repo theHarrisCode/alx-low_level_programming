0150314dac6f4b3c9858dddb68cd025ab191c735
Darren Harris <theharriscode401@gmail.com>
2023-06-12 17:21:37 -0400

    new updates

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints the program's name and new line
+ * @argc: amount of arguments in command line
+ * @argv: command line elements
  * Return: 0 Always (Success)
  */
 int main(int argc, char *argv[])
