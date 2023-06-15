c52afef8805f74a4022110e65203b4759daf7b29
Darren Harris <theharriscode401@gmail.com>
2023-06-15 11:33:43 -0700

    funciton that multiplies integer

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments in command line
+ * @argv: argument vector, one dimensional array
+ * Return: 0 Always (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int int1, int2, sum;
+
+	int1 = atoi(argv[1]);
+	int2 = atoi(argv[2]);
+	sum = int1 + int2;
+	printf("%d\n", sum);
+	(void)argc;
+	return (0);
+}
