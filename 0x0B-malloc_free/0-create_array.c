e5c802b257fbd5df5a4cebc227bcb9ba876d7d39
Darren Harris <theharriscode401@gmail.com>
2023-06-16 07:54:40 -0700

    array, casting malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	else if (c || c == '\0')
 	{
-		ar = (char*)malloc(size * sizeof(char));
+		ar = (char *)malloc(size * sizeof(char));
 		for (i = 0; i < size; i++)
 		{
 			*(ar + i) = c;
