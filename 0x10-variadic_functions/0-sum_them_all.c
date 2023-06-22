3f35e67e6df405e6da5df5a173c19c0f63d622a1
Darren Harris <theharriscode401@gmail.com>
2023-06-22 12:08:25 -0700

    adding all parameters in function using variadic funciton

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sums up all of the integers in the parameter
+ * @n: number to be added to sum
+ * Return: sum of all paramters, 0 if n = 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(nums, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(nums, int);
+	va_end(nums);
+	return sum;
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,5 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCITONS_H
+
+int sum_them_all(const unsigned int n, ...);
+#endif
