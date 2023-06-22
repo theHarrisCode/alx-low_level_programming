f7a8bfb6c098a9fe4ba91e7d679e790ed909f31a
Darren Harris <theharriscode401@gmail.com>
2023-06-22 11:29:33 -0700

    function that checks array for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,16 +7,16 @@
  * @cmp: pointer to a function
  * Return: @i if element found, -1 if fail
  */
-int int_index (int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (size <= 0)
-		return -1;
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			return i;
+			return (i);
 	}
-	return -1;
+	return (-1);
 }
