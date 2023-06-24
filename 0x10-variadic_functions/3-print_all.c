a635bbe13a5ceb7f862d11e9818dd054d7daa743
Darren Harris <theharriscode401@gmail.com>
2023-06-23 18:01:14 -0700

    created variadic function that prints all the arguments

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,4 +8,39 @@
 void print_all(const char * const format, ...)
 {
 	va_list argmt;
-	
+	int n = 0;
+	char *str, *separator = "";
+
+	va_start(argmt, format);
+	if (format)
+	{
+		while (*(format + n))
+		{
+			switch (*(format + n))
+			{
+			case 'c':
+				printf("%s%c", separator, va_arg(argmt, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(argmt, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(argmt, double));
+				break;
+			case 's':
+				str = va_arg(argmt, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				break;
+			default:
+				n++;
+				continue;
+			}
+			n++;
+			separator = ", ";
+		}
+	}
+	printf("\n");
+	va_end(argmt);
+}
