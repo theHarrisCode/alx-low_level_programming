0fea3fe7eef639f4b019ec7372de1506c7090df1
Darren Harris <theharriscode401@gmail.com>
2023-06-27 14:27:19 -0400

    added the stdio library to 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
 /**
  * init_dog - function that initializes a variable of type struct dog
@@ -10,11 +11,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if(d == NULL)
-		;
-	else
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
