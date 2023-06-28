166cf0bd90686d1a0264a7a9059d4ef5f45fca56
Darren Harris <theharriscode401@gmail.com>
2023-06-28 13:23:07 -0700

    new chagnes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,21 +6,24 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-	printf("Name: ");
-	if (d->name == NULL)
-		printf("(nil)\n");
+	if (d != NULL)
+	{
+		printf("Name: ");
+		if (d->name == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", d->name);
+		printf("Age: ");
+		if (!d->age)
+			printf("(nil)\n");
+		else
+			printf("%f\n", d->age);
+		printf("Owner: ");
+		if (d->owner == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", d->owner);
+	}
 	else
-		printf("%s\n", d->name);
-	printf("Age: ");
-	if (!d->age)
-		printf("(nil)\n");
-	else
-		printf("%f\n", d->age);
-	printf("Owner: ");
-	if (d->owner == NULL)
-		printf("(nil)\n");
-	else
-		printf("%s\n", d->owner);
+		printf("");
 }
