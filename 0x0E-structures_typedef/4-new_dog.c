ffdeca25ca634d841f5c404777af5936c5676a7d
Darren Harris <theharriscode401@gmail.com>
2023-06-28 13:46:24 -0700

    fixed betty problem

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,47 +12,56 @@ char *_strcpy(char *src, char *dest);
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *d;
-    int name_length, owner_length;
-    /*Add 1 b/c in _strlen we only count until the string terminator char*/
-    if (name != NULL && owner != NULL)
-    {
-        name_length = _strlen(name) + 1;
-        owner_length = _strlen(owner) + 1;
-        d = malloc(sizeof(dog_t));
-        if (d == NULL)
-            return (NULL);
-        d->name = malloc(sizeof(char) * name_length);
-        if (!d->name)
-        {
-            free(d);
-            return (NULL);
-        }
-        _strcpy(d->name, name);
-        d->age = age;
-        d->owner = malloc(sizeof(char) * owner_length);
-        if(!d->owner)
-        {
-            free(d->name);
-            free(d);
-            return (NULL);
-        }
-        _strcpy(d->owner, owner);
-        return (d);
-    }
-    return (NULL);
-}
+	dog_t *d;
+	int name_length, owner_length;
 
+	if (name != NULL && owner != NULL)
+	{
+		name_length = _strlen(name) + 1;
+		owner_length = _strlen(owner) + 1;
+		d = malloc(sizeof(dog_t));
+		if (d == NULL)
+			return (NULL);
+		d->name = malloc(sizeof(char) * name_length);
+		if (!d->name)
+		{
+			free(d);
+			return (NULL);
+		}
+		_strcpy(d->name, name);
+		d->age = age;
+		d->owner = malloc(sizeof(char) * owner_length);
+		if (!d->owner)
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
+		_strcpy(d->owner, owner);
+		return (d);
+	}
+	return (NULL);
+}
+/**
+ * _strlen - counts the length of a string
+ * @s: string to be counted
+ * Return: the legnth of the string
+ */
 int _strlen(char *s)
 {
-    int i, j;
+	int i, j;
 
-    j = 0;
-    for(i = 0; *(s + i) != '\0'; i++)
-	    j++;
-    return(j);
+	j = 0;
+	for (i = 0; *(s + i) != '\0'; i++)
+		j++;
+	return (j);
 }
-
+/**
+ * _strcpy - copies from source to destination
+ * @dest: where the string will be copied to
+ * @src: the string to be copied
+ * Return: pointer to the dest string
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
