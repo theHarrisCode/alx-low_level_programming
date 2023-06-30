94eb88653d4185f4782c65f0c9c1e35683c1c3f5
Darren Harris <theharriscode401@gmail.com>
2023-06-30 12:36:00 -0700

    cleaning code for 2-add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,7 +5,7 @@
 /**
  * add_node - adds a new node to the beginning of the lists
  * @head: double pointer to head of list
- * @str" string to add to list
+ * @str: string to add to list
  * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
