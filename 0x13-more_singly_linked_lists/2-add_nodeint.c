e3047f64429267f0a183cfa321d7ed222fe7ab89
Darren Harris <theharriscode401@gmail.com>
2023-07-04 12:39:20 -0700

    betty fix

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 	new->n = n;
-        new->next = *head;
+	new->next = *head;
 	*head = new;
 	if (new == NULL)
 		return (NULL);
